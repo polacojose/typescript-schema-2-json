92b4b190a2fec1a275858b59fe258b7bda783cfb
Jose Polaco <polacojose@gmail.com>
2023-07-10 19:28:19 +1200

    Added distributable

---
+++ pkg/typescript_schema_2_json.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} file_contents
+* @returns {any}
+*/
+export function parse(file_contents: string): any;
---
+++ pkg/typescript_schema_2_json_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function parse(a: number, b: number): number;
+export function __wbindgen_export_0(a: number, b: number): number;
+export function __wbindgen_export_1(a: number, b: number, c: number, d: number): number;
